2d14303ee24d408d1c4e4a9d8f115e9df6181264
Mateusz L <mateusz.l@1000i.pl>
2014-05-06 00:59:24 +0200

    Initial rails app

---
+++ rasp-rails/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RaspRails::Application.config.secret_token = 'b018e51ab47a7d1e60e40c723aa0bc814f342dd4bd5e83190c4d5013a2b08ad477b2bcaf5827d374e3a4a0777f3e71200789ce2bcbf71f64f656b01870c43dcc'
