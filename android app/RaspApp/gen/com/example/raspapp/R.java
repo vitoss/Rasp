2c0cac00f17f086beb3ab079284d73bad2153e76
bonzai1990 <mateusz.bacal90@o2.pl>
2014-05-07 23:25:46 +0200

    Json Object
    Method of checking is on mobile phone is available internet connection.
    Create initial version of method to retrieve JsonObjects and create
    results - in progress

--- android app/RaspApp/gen/com/example/raspapp/R.java
+++ android app/RaspApp/gen/com/example/raspapp/R.java
@@ -982,6 +982,9 @@ containing a value of this type.
         public static final int abc_textfield_searchview_right_holo_dark=0x7f020055;
         public static final int abc_textfield_searchview_right_holo_light=0x7f020056;
         public static final int ic_launcher=0x7f020057;
+        public static final int offline=0x7f020058;
+        public static final int onlinenow=0x7f020059;
+        public static final int onlinenowsmall=0x7f02005a;
     }
     public static final class id {
         public static final int action_bar=0x7f05001c;
@@ -995,7 +998,8 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f05003d;
+        public static final int action_search=0x7f05003f;
+        public static final int action_settings=0x7f05003e;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
@@ -1006,6 +1010,7 @@ containing a value of this type.
         public static final int dialog=0x7f05000e;
         public static final int disableHome=0x7f050008;
         public static final int dropdown=0x7f05000f;
+        public static final int edit_message=0x7f05003d;
         public static final int edit_query=0x7f050030;
         public static final int end=0x7f050013;
         public static final int expand_activities_button=0x7f050026;
@@ -1096,12 +1101,15 @@ containing a value of this type.
         public static final int abc_popup_menu_item_layout=0x7f030014;
         public static final int abc_search_dropdown_item_icons_2line=0x7f030015;
         public static final int abc_search_view=0x7f030016;
-        public static final int activity_main=0x7f030017;
-        public static final int fragment_main=0x7f030018;
-        public static final int support_simple_spinner_dropdown_item=0x7f030019;
+        public static final int activity_display_message=0x7f030017;
+        public static final int activity_main=0x7f030018;
+        public static final int fragment_display_message=0x7f030019;
+        public static final int fragment_main=0x7f03001a;
+        public static final int support_simple_spinner_dropdown_item=0x7f03001b;
     }
     public static final class menu {
-        public static final int main=0x7f0c0000;
+        public static final int display_message=0x7f0c0000;
+        public static final int main=0x7f0c0001;
     }
     public static final class string {
         /**  Content description for the action bar "home" affordance. [CHAR LIMIT=NONE] 
@@ -1144,9 +1152,13 @@ containing a value of this type.
         /**  Description of a share target (both in the list of such or the default share button) in a ShareActionProvider (share UI). [CHAR LIMIT=NONE] 
          */
         public static final int abc_shareactionprovider_share_with_application=0x7f0a000b;
-        public static final int action_settings=0x7f0a000f;
+        public static final int action_search=0x7f0a000f;
+        public static final int action_settings=0x7f0a000e;
         public static final int app_name=0x7f0a000d;
-        public static final int hello_world=0x7f0a000e;
+        public static final int button_send=0x7f0a0011;
+        public static final int edit_message=0x7f0a0010;
+        public static final int hello_world=0x7f0a0013;
+        public static final int title_activity_display_message=0x7f0a0012;
     }
     public static final class style {
         /** 
